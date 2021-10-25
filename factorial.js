1f790bb58153fa05959b38f5aa62faf8371ded03
Rakib <rakib@Rakibs-MacBook-Pro.local>
2021-10-26 00:35:01 +0600

    factorial

---
+++ factorial.js
@@ -0,0 +1,14 @@
+// let factorial = 1;
+// for (var i = 1; i <5;i++){
+//     factorial = factorial *i;
+//     console.log(i,factorial);
+// }
+function factorial(n){
+    let factorial = 1;
+for (var i = 1; i <n;i++){
+    factorial = factorial *i;
+}
+return factorial;
+}
+var result = factorial(10);
+console.log(result);
